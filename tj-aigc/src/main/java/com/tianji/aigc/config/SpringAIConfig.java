232819bb06bcfdab91a5d64a2b60974d641fa93b
Kevin <hello@itcast.cn>
2025-12-07 13:36:01 +0800

    调用AI根据课程ID查询课程业务实现

--- tj-aigc/src/main/java/com/tianji/aigc/config/SpringAIConfig.java
+++ tj-aigc/src/main/java/com/tianji/aigc/config/SpringAIConfig.java
@@ -2,6 +2,7 @@ package com.tianji.aigc.config;
 
 import com.tianji.aigc.memory.HybridChatMemory;
 import com.tianji.aigc.memory.RedisChatMemory;
+import com.tianji.aigc.tools.CourseTools;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -22,9 +23,11 @@ public class SpringAIConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder,
                                  Advisor loggerAdvisor,
-                                 Advisor messageChatMemoryAdvisor) {  // 日志记录器
+                                 Advisor messageChatMemoryAdvisor,
+                                 CourseTools courseTools) {  // 日志记录器
         return chatClientBuilder
-                .defaultAdvisors(loggerAdvisor,messageChatMemoryAdvisor) //添加 Advisor 功能增强
+                .defaultAdvisors(loggerAdvisor, messageChatMemoryAdvisor) //添加 Advisor 功能增强
+                .defaultTools(courseTools) //添加默认工具
                 .build();
     }
 
